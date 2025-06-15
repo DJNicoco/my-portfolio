08a053f73fcfc6886155efdc4eac1268fb156d9b
itzzzznicoleeeee <67239099+DJNicoco@users.noreply.github.com>
2025-06-15 08:28:16 -0700

    Made a style change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,9 @@
 const Navbar = ({ toggleTheme, currentTheme }) => {
   return (
     <nav style={{ padding: '1rem', display: 'flex', justifyContent: 'space-between' }}>
-      <h2>Nicole Zino</h2>
-      <div>
         <button onClick={toggleTheme}>
           {currentTheme === 'light' ? '🌙 Dark' : '☀️ Light'}
         </button>
-      </div>
     </nav>
   );
 };
