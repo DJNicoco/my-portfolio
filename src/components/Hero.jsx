7d6288c6a68a663ea4da66ffda9375d7a4a70cd8
itzzzznicoleeeee <67239099+DJNicoco@users.noreply.github.com>
2025-06-14 22:56:44 -0700

    Edited page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,8 @@ const Hero = () => {
     <section>
       <h1>Hello, I'm Nicole Zino 👩‍💻</h1>
       <p>
-        I'm a Computer Science major passionate about solving real-world problems with clean,
-        efficient code. Welcome to my portfolio!
+        I'm a Computer Science major passionate about solving real-world problems with efficient code. Welcome to my portfolio!
       </p>
-      <a href="#contact">📬 Let’s Connect</a>
     </section>
   );
 };
